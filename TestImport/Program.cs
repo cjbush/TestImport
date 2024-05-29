3447c4e934763a88b317052446d5bfe04547e536
cjbus <cjbus@CHRIS-DESKTOP>
2024-05-28 23:01:07 -0500

    Add project files.

---
+++ TestImport/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+
+	[DllImport("TestExport.dll")]
+	public static extern int Add(int a, int b);
+
+	private static void Main(string[] args)
+	{
+		Console.WriteLine(Add(2, 3));
+	}
+}
